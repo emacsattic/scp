59d9164741960bb63a4394e7efe480f4901b8924
zg <1181736238@qq.com>
2017-12-02 15:59:02 +0800

    Revert "Revert "Use the shell-quot-argument processing parameters""
    This reverts commit 63bb1e67cb544dd54b3e9f9adec0d1c7b94fabea.

--- scp.el
+++ scp.el
@@ -51,19 +51,15 @@
 
 (cl-defun scp-get-alist(key &optional (alist file-local-variables-alist))
   "Gets the value of the Association List"
-  (cdr (assoc key alist)))
+  (shell-quote-argument (cdr (assoc key alist))))
 
 (defun scp-cmd(status &optional local-path)
   "Stitching scp command"
   (hack-dir-local-variables)
-  (let* ((host
-	  (shell-quote-argument (scp-get-alist 'host)))
-	 (user
-	  (shell-quote-argument (scp-get-alist 'user)))
-	 (port
-	  (shell-quote-argument (scp-get-alist 'port)))
-	 (pw
-	  (shell-quote-argument (scp-get-alist 'password)))
+  (let* ((host (scp-get-alist 'host))
+	 (user (scp-get-alist 'user))
+	 (port (scp-get-alist 'port))
+	 (pw (scp-get-alist 'password))
 	 (cmd (concat (unless (memq system-type '(windows-nt ms-dos))
 			(format "sshpass -p %s " pw))
 		      scp-tools  (unless (eq local-path buffer-file-name) " -r")
